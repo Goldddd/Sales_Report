31b03ea75631b6fd07220ba4d1ac986bf23dadb7
xau <65294921+Goldddd@users.noreply.github.com>
2022-03-16 09:48:57 +0100

    Add files via upload

---
+++ SQLQuery CleanedcustomerTable.sql
@@ -0,0 +1,14 @@
+-- Cleansed DIM_Customers Table --
+SELECT 
+  c.customerkey AS CustomerKey, 
+  c.firstname AS [First Name], 
+  c.lastname AS [Last Name], 
+  c.firstname + ' ' + lastname AS [Full Name], 
+  CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS Gender,
+  c.datefirstpurchase AS DateFirstPurchase, 
+  g.city AS [Customer City] -- Joined in Customer City from Geography Table
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimCustomer] as c
+  LEFT JOIN dbo.dimgeography AS g ON g.geographykey = c.geographykey 
+ORDER BY 
+  CustomerKey ASC -- Ordered List by CustomerKey
---
+++ SQLQuery cleanedSalesTABLE.sql
@@ -0,0 +1,15 @@
+-- Cleansed FACT_InternetSales Table --
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  [SalesOrderNumber], 
+  [SalesAmount] --  ,[TaxAmt]
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2 -- Ensures we always only bring two years of date from extraction.
+ORDER BY
+  OrderDateKey ASC
